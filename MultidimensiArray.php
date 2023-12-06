8f2f67eaee08828835785e2269e029178ea433b5
Hafidz Fatah K.J <144297795+zyhapis@users.noreply.github.com>
2023-12-06 10:33:19 +0700

    Update MultidimensiArray.php

--- MultidimensiArray.php
+++ MultidimensiArray.php
@@ -8,7 +8,7 @@
     ];
 
     // menampilkan array kendaraan
-    echo "Ini Array Kendaraan <br>";
+    echo "Ini Array Kendaraan <br><br>";
     $output = "";
     foreach ($kendaraan["transportasi"] as $value) {
         $output .= $value . ", ";
@@ -16,10 +16,10 @@
 
     $output = rtrim($output, ", ");
     echo $output . "<br>";
-    echo "====================<br>";
+    echo "=========================<br>";
 
     // menampilkan array mobil
-    echo "Ini Array Mobil <br>";
+    echo "Ini Array Mobil <br><br>";
     $output = "";
     foreach ($kendaraan["mobil"] as $value) {
         $output .= $value . ", ";
@@ -27,10 +27,10 @@
 
     $output = rtrim($output, ", ");
     echo $output . "<br>";
-    echo "====================<br>";
+    echo "=========================<br>";
 
     // menampilkan array motor
-    echo "Ini Array Motor <br>";
+    echo "Ini Array Motor <br><br>";
     $output = "";
     foreach ($kendaraan["motor"] as $value) {
         $output .= $value . ", ";
